83cd9c8b5de30c4475144ab42f5fcaa7984aa58e
Ragul <inforagul4@gmail.com>
2026-01-09 14:54:42 +0530

    changes added in hostfix.

--- train.java
+++ train.java
@@ -5,6 +5,7 @@ public class train {
     String destination;
     int totalSeats;
     int availableSeats;
+    // Fare per ticket
     double fare;
 
     train(int trainID, String trainName, String source, String destination, int totalSeats, int availableSeats, double fare) {
@@ -12,6 +13,7 @@ public class train {
         this.trainName = trainName;
         this.source = source;
         this.destination = destination;
+
         this.totalSeats = totalSeats;
         this.availableSeats = availableSeats;
         this.fare = fare;
@@ -19,11 +21,12 @@ public class train {
 
     void displaytrainInfo() {
         System.out.println("Train ID : " + trainID);
+
         System.out.println("Name     : " + trainName);
         System.out.println("Route    : " + source + " → " + destination);
         System.out.println("Seats    : " + availableSeats + "/" + totalSeats);
         System.out.println("Fare     : ₹" + fare);
-        System.out.println("---------------------------------");
+        System.out.println("-----------------------------------------------------------------");
     }
 }
 
